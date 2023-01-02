22e6713951f9100a1fd62a8f3d6024584b6c04e9
RahulJain <rj.rahul251998@gmail.com>
2023-01-02 16:41:55 +0530

    commited

--- demo.py
+++ demo.py
@@ -1,7 +1,4 @@
 print("Hello world")
 print("Hello Everyone")
-<<<<<<< HEAD
-print("line 3 on local")
-=======
 print("line 3 on Github")
->>>>>>> 7580d7a50600decb66eab803ce7c170d881e3a62
+print("line 4 on local")
