ee14c52140b3a8925cfe4e3ba7ef3c42f52e1930
RahulJain <rj.rahul251998@gmail.com>
2023-01-02 16:59:25 +0530

    commit

--- demo.py
+++ demo.py
@@ -2,9 +2,5 @@ print("Hello world")
 print("Hello Everyone")
 print("line 3 on Github")
 print("line 4 on local")
-<<<<<<< HEAD
 print("line 5 in local")
-=======
-print("line 5 on github")
->>>>>>> 3dafa3b18a58e2e5731d5ba79de587a2ca080e31
-print("line 6 on git")
+print("line 6 on local")
